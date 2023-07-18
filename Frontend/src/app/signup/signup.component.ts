c021a9503b1548a0d0948e55c60c7ac2db6aba65
Nguyen Quoc Khanh <quockhanhnguyen2882@gmail.com>
2023-07-18 18:03:37 +0700

    sign up frontend

--- Frontend/src/app/signup/signup.component.ts
+++ Frontend/src/app/signup/signup.component.ts
@@ -1,47 +1,44 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit, AfterViewInit{
+export class SignupComponent implements OnInit{
 
-  signUpForm: FormGroup = new FormGroup({
-    name: new FormControl(''),  
-    email: new FormControl(''),
-    password: new FormControl(''),
-    contactNumber: new FormControl('')
-  });
-
-  constructor(private userService: UserService, private formBuilder: FormBuilder){
+  signUpForm: any = {
+    name: "",
+    email: "",
+    password: "",
+    contactNumber: ""
   }
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  ngAfterViewInit(): void { 
-  } 
+  constructor(private authService: AuthService){
+  }
 
   ngOnInit(): void {   
   }
 
-  public signUp(){
-    if (this.signUpForm.valid) {
-      console.log(this.signUpForm.value);
-      this.signUpForm.value.status = 'false';
-      this.signUpForm.value.role = 'user';
-      console.log(this.signUpForm.value);
-      const requestData = this.signUpForm.value;
-      this.userService.signUp(requestData)
-        .subscribe(
-          (response) => {
-            console.log('Đăng ký thành công');
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-    }
+  onSubmit(){
+    console.log(this.signUpForm);
+    this.authService.signUp(this.signUpForm).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    })
   }
+
+  
 
 }
